81b9f5974bfe1a7acb2bab4e4c073f213b978127
Mrproliu <mrproliu@lagou.com>
2020-12-19 00:28:58 +0800

    Using multi-thread for benchmark

--- apm-commons/apm-datacarrier/src/test/java/org/apache/skywalking/apm/commons/datacarrier/DataCarrierBenchmark.java
+++ apm-commons/apm-datacarrier/src/test/java/org/apache/skywalking/apm/commons/datacarrier/DataCarrierBenchmark.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.skywalking.apm.commons.datacarrier;
 
 import com.lmax.disruptor.EventHandler;
@@ -11,14 +29,15 @@ import org.apache.skywalking.apm.commons.datacarrier.buffer.BufferStrategy;
 import org.apache.skywalking.apm.commons.datacarrier.consumer.IConsumer;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Group;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -28,8 +47,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-@State(Scope.Thread)
+@BenchmarkMode({Mode.Throughput})
+@State(Scope.Benchmark)
 public class DataCarrierBenchmark {
 
     private RingBuffer<StringWrapper> ringBuffer;
@@ -39,17 +58,17 @@ public class DataCarrierBenchmark {
     @Param({"BLOCKING", "IF_POSSIBLE"})
     private BufferStrategy strategy;
 
-    @Param({"512", "1024", "2048"})
+    @Param({"512", "1024", "8192"})
     private int bufferSize;
 
-    @Param({"1", "10", "20"})
+    @Param({"1", "5", "10"})
     private int consumerCount;
 
     @Setup
     public void setup(Blackhole blackhole) {
         // Init Disruptor
         disruptor = new Disruptor<>(StringWrapper::new, bufferSize, DaemonThreadFactory.INSTANCE,
-            ProducerType.SINGLE, new SleepingWaitStrategy(0, 20));
+            ProducerType.MULTI, new SleepingWaitStrategy(0, 20));
         ringBuffer = disruptor.getRingBuffer();
         disruptor.handleEventsWith(IntStream.range(0, consumerCount).mapToObj(i -> (EventHandler<StringWrapper>) (event, sequence, endOfBatch) -> {
             blackhole.consume(event);
@@ -57,13 +76,13 @@ public class DataCarrierBenchmark {
         disruptor.start();
 
         // Init DataCarrier
-        dataCarrier = new DataCarrier<>(10, bufferSize);
+        dataCarrier = new DataCarrier<>(5, bufferSize);
         dataCarrier.setBufferStrategy(strategy);
         dataCarrier.consume(new DataCarrierConsumer(blackhole), consumerCount);
     }
 
     @Benchmark
-    @Group("DisruptorAndDataCarrier")
+    @Threads(5)
     public void disruptor(Blackhole blackhole) {
         try {
             long inx = strategy == BufferStrategy.IF_POSSIBLE ? ringBuffer.tryNext() : ringBuffer.next();
@@ -75,7 +94,7 @@ public class DataCarrierBenchmark {
     }
 
     @Benchmark
-    @Group("DisruptorAndDataCarrier")
+    @Threads(5)
     public void dataCarrier(Blackhole blackhole) {
         if (dataCarrier.produce("")) {
             blackhole.consume(true);
@@ -103,8 +122,8 @@ public class DataCarrierBenchmark {
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
             .include(DataCarrierBenchmark.class.getSimpleName())
-            .jvmArgsAppend("-Xmx512m", "-Xms512m")
-            .forks(1)
+            .result("result.json")
+            .resultFormat(ResultFormatType.JSON)
             .build();
 
         new Runner(opt).run();
